2b88c968878bf4177b9c1a48bb7e87fc003ba9f2
Proxypro2012 <kabir.tiwari@outlook.com>
2025-02-02 22:02:14 -0500

    Update attendanceauto.py

--- attendanceauto.py
+++ attendanceauto.py
@@ -4,7 +4,8 @@ import json
 # Simulate receiving JSON data from query parameters
 st.title("Streamlit JSON Receiver")
 
-query_params = st.query_params()
+# Get the query parameters using experimental API
+query_params = st.experimental_get_query_params()
 
 # Get JSON from query param 'data'
 json_data = query_params.get('data', [None])[0]
@@ -18,5 +19,3 @@ if json_data:
         st.write("Invalid JSON data.")
 else:
     st.write("No JSON data received.")
-
-
